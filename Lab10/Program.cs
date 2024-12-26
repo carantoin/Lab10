b37733504db70cdb4bde7aa473d5a4698ca1442b
carantoin <90127329+carantoin@users.noreply.github.com>
2024-12-26 20:16:08 +0200

    Добавьте файлы проекта.

---
+++ Lab10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data;
+
+public class Year
+{
+    public int YearOfRelease { get; set; }
+    public Year(int yearOfRelease)
+    {
+        YearOfRelease = yearOfRelease;
+    }
+    public int GetYear()
+    {
+        return DataSetDateTime.Now.Year;
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите год выпуска товара:");
+        double y = double.Parse(Console.ReadLine());
+        Year year = new Year();
+    }
+}
